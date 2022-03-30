c6bdeadef7d00115baecf32d5a5d13fd58cd62cf
Michael <michael@ou.edu>
2022-03-29 20:55:38 -0500

    Finished c1 and c2

--- C1.py
+++ C1.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+#MATH 4073 Homework 9 - Michael Bartlett
+import numpy as np
+def euler_method(f, a, b, alpha, steps):
+    '''
+    Function that estimates the solution of a differential equation using Euler's method
+    :param f: The function that is equal to x'(t). Function of the form f(x, t)
+    :param a: The initial time
+    :param b: The final time
+    :param alpha: The initial value; x(a) = alpha
+    :param steps: The number of steps to use
+    :return: A list containing the estimated function values
+    '''
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    #Define the step size and initial solution list
+    h = (b-a) / steps
+    solution = []
 
+    #Set the inital values
+    t = a
+    x = alpha
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    for i in range(steps+1): #Add one to make sure we calculate the value at the final time
+        #Add the solution to the solution list
+        solution.append(x)
 
+        #Update the values of x and t
+        x = x + h*f(x, t)
+        t = a + h * i
+
+    return solution
+
+def c2_a(x, t):
+    return 1 + (t-x)**2
+
+def c2_b(x, t):
+    return t**(-2) * (np.sin(2*t) - 2*t*x)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("C2a solution:")
+    for i in euler_method(c2_a, 2, 3, 1, 20):
+        print(i)
+
+    print("C2b solution:")
+    for i in euler_method(c2_b, 1, 2, 2, 40):
+        print(i)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
